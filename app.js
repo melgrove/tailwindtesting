c1c2e3c76a2ced9b32407874261d452841418190
Oliver Melgrove <oliver@olivermac.local>
2020-06-21 01:46:03 -0700

    added side panel and footer

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
-const express = require('express')
+const express = require('express');
+const bcrypt = require('bcrypt');
+const Cookies = require('cookies');
 const app = express()
-const port = 2900 
+const port = 3000
+
+let keys = ['thisismykey'];
 
 app.use(express.static(__dirname + '/public'))
+
+app.get('/login', [login, sendFile]);
+
 app.get('/main', (req, res) => {
     res.sendFile(__dirname + '/main.html');
 });
-    
+
 app.listen(port, () => console.log(`Express server running on port ${port}`));
+     
+    
+function login(req, res, next) {
+    let cookies = new Cookies(req, res, {keys: keys})
+    res.locals.lastVisit = cookies.get('lastVisit', { signed: true })
+    cookies.set('lastVisit', new Date(), { signed: true })
+    if(!res.locals.lastVisit) {
+        res.locals.login = true
+    } else {
+        res.locals.login = false
+    }
+    next();
+};
+
+function sendFile(req, res) {
+    if(res.locals.login) {
+        res.send("Welome stranger")
+        //res.sendFile(__dirname + '/main.html');
+    } else {
+        res.send(`Welcome back. You're cookie is ${res.locals.lastVisit}`)
+    }
+    
+};
+ 
+  
